40b390a543046316f3080ab060e44027c2c239bf
Kamogelo-06 <kamomakwela06@gmail.com>
2025-02-10 19:26:20 +0200

    Add files via upload

---
+++ conversion.py.py
@@ -0,0 +1,23 @@
+# This Program demostrates variables declarations and type conversion in Python.
+
+# Step: 1 Declare the given given variables
+num1 = 99.23  
+num2 = 23    
+num3 = 150    
+string1 = "100"
+
+# Step: 2 Convert variables to different data types
+num1_int = int(num1)
+num2_float = float(num2)
+num3_str = str(num3)
+string1_int = int(string1)
+
+# Step: 3 Print  the results  to verify  the conversion
+print("Original num1:",num1) # This output the original  float value
+print("Converted num1 to integer:",num1_int) # This will output  the converted  integer value
+print("Original num2:",num2) # This output the original integer value
+print("Converted num2 to float:",num2_float ) # This will output the converted float value
+print("Original num3:",num3) # This will output the original integer
+print("Converted num3 to string:",num3_str) # This will output the converted string value
+print("Original string1:",string1) # This will output the original string value
+print("Converted string1 to integer:",string1_int) # This will output the converted integer value
---
+++ hello_world.py.py
@@ -0,0 +1,9 @@
+# Get user information.
+
+# Step 1: Get user's name and age.
+user_name= input("Name:")
+
+user_age= input("Age:")
+
+# Step 2: Print the string "Hello World!"
+print("Hello World!")
