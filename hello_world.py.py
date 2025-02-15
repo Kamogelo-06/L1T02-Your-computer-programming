11d063d6936a1defcbe8e35dbdea2252b9cf6d4d
Kamogelo-06 <kamomakwela06@gmail.com>
2025-02-15 10:46:57 +0200

    Delete hello_world.py.py

--- hello_world.py.py
+++
@@ -1,9 +0,0 @@
-# Get user information.
-
-# Step 1: Get user's name and age.
-user_name= input("Name:")
-
-user_age= input("Age:")
-
-# Step 2: Print the string "Hello World!"
-print("Hello World!")
